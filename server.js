e2991b84780dd4db51d6edf06e5a1fd5b07c1dc8
phhien203 <phhien203@gmail.com>
2015-01-21 20:56:34 +0700

    Change the database config

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ app.set('port', process.env.PORT || 3300);
 app.set('views', __dirname + '/views');
 app = config(app);
 
-mongoose.connect('mongodb://localhost/imgPloadr');
+//mongoose.connect('mongodb://localhost/imgPloadr');
+mongoose.connect('mongodb://nodejitsu:180619b6b49e7a1ba4101b96d263e2da@troup.mongohq.com:10090/nodejitsudb192674293');
 mongoose.connection.on('open', function() {
     console.log('Mongoose connected.');
 });
